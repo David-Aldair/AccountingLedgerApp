a8791bb1bf5d460f4acbe98839e91ad375f47803
David-Aldair <d.aldair.quinonez@gmail.com>
2025-10-14 14:14:22 -0400

    Created a transactions class that represents the transactions recorded.

---
+++ src/main/java/com/pluralsight/Transactions.java
@@ -0,0 +1,14 @@
+package com.pluralsight;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+//this transactions class represents a transaction recorded
+public class Transactions {
+    private LocalDate date;
+    private LocalTime time;
+    private String description;
+    private String vendor;
+    private double amount;
+
+}
